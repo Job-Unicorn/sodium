ca3d5f418681b48a9d0594bdfb2bbad83ee4b7ac
aarush-goyal <goyalaarush92@gmail.com>
2021-10-01 02:09:48 +0530

    refactor[hooks]: removed unused imports

--- store/hooks/useChatState.ts
+++ store/hooks/useChatState.ts
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react"
+import { useReducer } from "react"
 import { ChatInitialState } from "@/store/state/chat"
 import { chatReducer } from "@/store/reducers/ChatReducer"
 
--- store/hooks/useJobsState.ts
+++ store/hooks/useJobsState.ts
@@ -3,8 +3,6 @@ import { JobsContextInterface } from "@/store/contexts/JobsContext"
 import { JobsReducer } from "@/store/reducers/JobsReducer"
 import { JobsInitialState } from "@/store/state/jobs"
 
-  
-
 export const useJobsState = () : JobsContextInterface => {
   const [jobsState, jobsDispatch] = useReducer(JobsReducer, JobsInitialState)
   return { jobsState, jobsDispatch }
